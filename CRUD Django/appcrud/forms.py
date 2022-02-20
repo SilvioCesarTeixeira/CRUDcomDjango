a829b0ce999e33b83949cd24a0dde19580413894
SilvioCesarTeixeira <91545032+SilvioCesarTeixeira@users.noreply.github.com>
2022-02-19 23:15:05 -0300

    Delete forms.py

--- CRUD Django/appcrud/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from appcrud.models import Reservas
-
-
-# Create the form class.
-class ReservasForm(ModelForm):
-    class Meta:
-        model = Reservas
-        fields = ['Req_nome', 'Mat_nro', 'Curso_nome', 'Sala_nro', 'Data', 'Horario', ]
